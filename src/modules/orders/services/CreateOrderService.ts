a4875a8adf34e1c6edbe0c40ed49730f57329d56
brunocheble <contato.cheble@gmail.com>
2020-08-21 21:58:34 +0100

    Conclusão do desafio 09 - Relacionamento com banco de dados no Node.js

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,74 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
     // TODO
+
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('This customer is not exist.');
+    }
+
+    const foundProducts = await this.productsRepository.findAllById(products);
+
+    if (!foundProducts.length) {
+      throw new AppError('Could not find any products with the given ids.');
+    }
+
+    const foundProductsIds = foundProducts.map(product => product.id);
+
+    const checkInexistentProducts = products.filter(
+      product => !foundProductsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id}`,
+      );
+    }
+
+    const foundProductsWithNoQuantityAvailable = products.filter(product => {
+      const foundProduct = foundProducts.find(p => p.id === product.id);
+      return !foundProduct || foundProduct.quantity < product.quantity;
+    });
+
+    if (foundProductsWithNoQuantityAvailable.length) {
+      throw new AppError(
+        `The quantity ${foundProductsWithNoQuantityAvailable[0].quantity} is not available for ${foundProductsWithNoQuantityAvailable[0].id}`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      price: foundProducts.filter(_product => _product.id === product.id)[0]
+        .price,
+      quantity: product.quantity,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: serializedProducts,
+    });
+
+    const orderedProductsQuantity = products.map(product => ({
+      id: product.id,
+      quantity:
+        foundProducts.filter(_product => _product.id === product.id)[0]
+          .quantity - product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,36 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    // DONE
+    const product = this.ormRepository.create({ name, price, quantity });
+    await this.ormRepository.save(product);
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    // DONE
+    const product = await this.ormRepository.findOne({
+      where: { name },
+    });
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    // DONE
+    const ids = products.map(product => product.id);
+    const findProducts = await this.ormRepository.find({
+      where: {
+        id: In(ids),
+      },
+    });
+    return findProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    // DONE
+    return this.ormRepository.save(products);
   }
 }
 
